3f57f288b083ef44da5469ea762cb98936896ca2
thesagarmahajan <sagarmahajan31997@gmail.com>
2021-10-27 13:51:23 +0530

    DateTime Completed

---
+++ Part-2/DateTime/DateTimesExamples.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import pytz
+# Aware and Naive objects
+# Aware: tzinfo is available
+# Naive: tzinfo is not available
+
+dt = datetime.now()
+print(dt)
+print(dt.tzinfo) # None i.e. Naive object
+
+tz = pytz.timezone('Asia/Kolkata')
+awaredt = tz.localize(dt)
+print(awaredt)
+# print(awaredt.tzinfo) # Asia/Kolkata i.e. Aware object
+print("--------------------------")
+print(awaredt.day)
+print(awaredt.month)
+print(awaredt.year)
+print(awaredt.hour)
+print(awaredt.minute)
+print(awaredt.second)
+print(awaredt.microsecond)
+print("%.0f"%awaredt.timestamp())
+print(awaredt.time())
