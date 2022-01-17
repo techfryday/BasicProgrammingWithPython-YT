50ab039b96f397958cddcdf1fb396082e6b8361c
thesagarmahajan <sagarmahajan31997@gmail.com>
2022-01-17 20:19:33 +0530

    Decorating RegEx

--- Part-2/RegEx/RegExExample.py
+++ Part-2/RegEx/RegExExample.py
@@ -13,14 +13,23 @@ $ => Ends with
 ( ) => group
 
 """
-txt=input("Enter your email: ")
-print("Is is an email? ")
+""" txt=input("Enter your email: ")
+print("Is is an email? ") """
 # isEmail = re.search("^[a-z0-9.]*@[a-z0-9]*\.com", txt)
 # isEmail = re.search("^[a-z0-9.]+@[a-z0-9]+\.com", txt)
 # print(re.match("^[a-z0-9.]+@[a-z0-9]+(.com|.in)$", txt)) 
-isEmail = re.search("^[a-z0-9.]{6,25}@[a-z0-9]+[a-z.]+", txt)
+""" isEmail = re.search("^[a-z0-9.]{6,25}@[a-z0-9]+[a-z.]+", txt)
 if isEmail:
     print("Yes it is!")
 else:
-    print("No its not!")
+    print("No its not!") """
+
+# match() - Determine if the RE matches at the beginning of the string
+line="1,sagar mahajan,sagar@gmail.com,1231231232"
+print(re.match("[0-9]+", line).group())
+
+# search() - Scan through a string, looking for any location where this RE matches
+# findall() - Find al substrings where RE matches and returns them as a list
+# finditer() - Find al substrings where RE matches and returns them as an iterator
+
 # https://docs.python.org/3/howto/regex.html#regex-howto
