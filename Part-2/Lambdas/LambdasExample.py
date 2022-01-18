a0d601119d43b5133f528d16bf1b1c353765b772
thesagarmahajan <sagarmahajan31997@gmail.com>
2022-01-18 18:45:12 +0530

    Lambdas Improved

--- Part-2/Lambdas/LambdasExample.py
+++ Part-2/Lambdas/LambdasExample.py
@@ -1,14 +1,16 @@
 # square = lambda n : n * n
 # print(square(12))
 
-# import re
-# isEmail = lambda str : re.match("^[a-z0-9.]+@[a-z0-9]+(.com|.in)$", str)
+# Example - 1
+""" import re
+isEmail = lambda str : re.match("^[a-z0-9.]+@[a-z0-9]+(.com|.in)$", str)
 
-# if(isEmail("sagar@gmail.com")):
-#     print("Correct")
-# else:
-#     print("Incorrect")
+if(isEmail("sagar@gmail.com")):
+    print("Correct")
+else:
+    print("Incorrect") """
 
-# a = [12,45,67,89,76,54,23,54,67]
-# newlist = list(filter(lambda n: n%2==0, a))
-# print(newlist)
+# Example - 2
+""" a = [12,45,67,89,76,54,23,54,67]
+newlist = list(filter(lambda n: n%2==0, a))
+print(newlist) """
