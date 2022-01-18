3f703a3d1beffc8662d989114bc9caa47f21a7c3
thesagarmahajan <sagarmahajan31997@gmail.com>
2022-01-18 18:44:58 +0530

    RegEx Practiced

--- Part-2/RegEx/RegExExample.py
+++ Part-2/RegEx/RegExExample.py
@@ -1,4 +1,5 @@
 import re
+
 """
 . => Any characters except the newline character
 ^ => Starts with
@@ -13,29 +14,35 @@ $ => Ends with
 ( ) => group
 
 """
-""" txt=input("Enter your email: ")
-print("Is is an email? ") """
-# isEmail = re.search("^[a-z0-9.]*@[a-z0-9]*\.com", txt)
-# isEmail = re.search("^[a-z0-9.]+@[a-z0-9]+\.com", txt)
-# print(re.match("^[a-z0-9.]+@[a-z0-9]+(.com|.in)$", txt)) 
-""" isEmail = re.search("^[a-z0-9.]{6,25}@[a-z0-9]+[a-z.]+", txt)
-if isEmail:
-    print("Yes it is!")
-else:
-    print("No its not!") """
+
 
 # match() - Determine if the RE matches at the beginning of the string
 line="1,sagar mahajan,sagar@gmail.com,1231231232"
-# print(re.match("[0-9]+", line).group())
+print(re.match("[0-9]+", line).group())
 
 # search() - Scan through a string, looking for any location where this RE matches
-# print(re.search("[0-9]{10}", line).group())
+print(re.search("[0-9]{10}", line).group())
 
 # findall() - Find al substrings where RE matches and returns them as a list
-# print(re.findall("[0-9]+", line))
+print(re.findall("[0-9]+", line))
 
 # finditer() - Find al substrings where RE matches and returns them as an iterator
 for i in re.finditer("[0-9]+", line):
     print(i.group())
 
+
+# Real-time Example
+
+txt=input("Enter your email: ")
+print("Is is an email? ")
+
+isEmail = re.search("^[a-z0-9.]+@[a-z0-9]+(.com|.in)$", txt)
+
+isEmail = re.search("^[a-z0-9.]{6,25}@[a-z0-9]+[a-z.]+", txt)
+if isEmail:
+    print("Yes it is!")
+else:
+    print("No its not!")
+    
+    
 # https://docs.python.org/3/howto/regex.html#regex-howto
